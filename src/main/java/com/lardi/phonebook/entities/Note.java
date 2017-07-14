9e5f7b3a391ce4fe6b672c70b9f35df935b38287
Maksim-ya <Maksim-ya@ukr.net>
2017-07-14 12:18:16 +0300

    DBconnect

---
+++ src/main/java/com/lardi/phonebook/entities/Note.java
@@ -0,0 +1,113 @@
+package com.lardi.phonebook.entities;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Notes")
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "Last_Name")
+    private String lastName;
+
+    @Column(name = "First_Name")
+    private String firstName;
+
+    @Column(name = "Middle_Name")
+    private String middleName;
+
+    @Column(name = "Mobile telephone")
+    private String telMobile;
+
+    @Column(name = "Domastic telephone")
+    private String telDomastic;
+
+    @Column(name = "Address")
+    private Double address;
+
+    @Column(name = "email")
+    private Double email;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getTelMobile() {
+        return telMobile;
+    }
+
+    public void setTelMobile(String telMobile) {
+        this.telMobile = telMobile;
+    }
+
+    public String getTelDomastic() {
+        return telDomastic;
+    }
+
+    public void setTelDomastic(String telDomastic) {
+        this.telDomastic = telDomastic;
+    }
+
+    public Double getAddress() {
+        return address;
+    }
+
+    public void setAddress(Double address) {
+        this.address = address;
+    }
+
+    public Double getEmail() {
+        return email;
+    }
+
+    public void setEmail(Double email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Note{" +
+                "id=" + id +
+                ", lastName='" + lastName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", telMobile='" + telMobile + '\'' +
+                ", telDomastic='" + telDomastic + '\'' +
+                ", address=" + address +
+                ", email=" + email +
+                '}';
+    }
+}
