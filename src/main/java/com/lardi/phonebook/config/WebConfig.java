fc7a76de2912cb315b939c23b39bdae59cbd798e
Maksim-ya <Maksim-ya@ukr.net>
2017-07-19 21:25:41 +0300

    ALL Fine with webapp (but not with notes)

--- src/main/java/com/lardi/phonebook/config/WebConfig.java
+++ src/main/java/com/lardi/phonebook/config/WebConfig.java
@@ -1,19 +1,21 @@
 package com.lardi.phonebook.config;
 
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+import org.thymeleaf.templateresolver.TemplateResolver;
 
-//@Configuration
-//public class WebConfig extends WebMvcConfigurerAdapter {
-//
-//    @Override
-//    public void configureViewResolvers(ViewResolverRegistry registry) {
-//        registry.jsp("/WEB-INF/views/", ".jspx");
-//    }
-//}
+
+
 
 
 @Configuration
@@ -21,13 +23,26 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addRedirectViewController("/", "/site/index");
+       registry.addRedirectViewController("/", "/site/index");
 
-        registry.addViewController("/site/index")
-                .setViewName("site/index");
+        registry.addViewController("/site/index");
+//                .setViewName("site/index");
+        registry.addViewController("/")
+               .setViewName("site/index");
+        registry.addViewController("site/notedata")
+                .setViewName("notedata");
+        registry.addViewController("site/notes")
+                .setViewName("notes");
     }
 
 }
+
+
+
+
+
+
+
 //@Bean
 //public ViewResolver viewResolver() {
 //    UrlBasedViewResolver urlBasedViewResolver = new UrlBasedViewResolver();
--- src/main/java/com/lardi/phonebook/controller/NoteController.java
+++ src/main/java/com/lardi/phonebook/controller/NoteController.java
@@ -2,37 +2,35 @@ package com.lardi.phonebook.controller;
 
 import com.lardi.phonebook.entities.Note;
 import com.lardi.phonebook.service.NoteService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class NoteController {
     private NoteService noteService;
 
 
-    @Autowired(required = true)
+    @Autowired()
     public void setNoteService (NoteService noteService){
         this.noteService= noteService;
     }
 
     @RequestMapping(value = "notes", method = RequestMethod.GET)
     public String listNotes(Model model) {
-        model.addAttribute("note", new Note());
+       // model.addAttribute("note", new Note());
         model.addAttribute("listNotes", this.noteService.listNotes());
-        return "note";
+        return "listNotes";
     }
-//    @RequestMapping(value = "/", method = RequestMethod.GET)
-//    public String index(Model model) {
-//        model.addAttribute("phoneBalances", (List<PhoneBalance>) phoneBalanceRepository.findByCurrent(true));
-//        return "index";
-//    }
+//
+//    @RequestMapping(value = "notes", method = RequestMethod.GET)
+//    public ModelAndView listOfNotes (ModelAndView modelAndView) {
+//        return modelAndView
+//                .addObject("note",new Note())
+//                .addObject("listNotes", this.noteService.listNotes());
+//        }
 
 
     @RequestMapping (value = "/notes/add", method = RequestMethod.POST)
