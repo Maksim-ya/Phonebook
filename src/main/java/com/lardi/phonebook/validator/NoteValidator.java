e7cd24e1fb36929cbff1ef161576a6d78ba28455
Maksim-ya <Maksim-ya@ukr.net>
2017-09-11 15:51:05 +0300

    10-09

--- src/main/java/com/lardi/phonebook/validator/NoteValidator.java
+++ src/main/java/com/lardi/phonebook/validator/NoteValidator.java
@@ -2,9 +2,8 @@ package com.lardi.phonebook.validator;
 
 
 import com.lardi.phonebook.model.Note;
-
-import org.springframework.stereotype.Component;
 import org.apache.commons.validator.routines.EmailValidator;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
@@ -19,29 +18,26 @@ public class NoteValidator implements Validator {
     }
 
     @Override
-     public void validate(Object o, Errors errors) {
+    public void validate(Object o, Errors errors) {
         Note note = (Note) o;
 
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "lastName", "required");
-        if (note.getLastName().length()<3){
+        if (note.getLastName().length() < 4) {
 
-            errors.rejectValue("lastName","size.message");
+            errors.rejectValue("lastName", "size.message");
         }
 
 
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "telMobile", "required");
-        if (!note.getTelMobile().matches("^((\\+?380)([5-9]{1})([0&3-9]{1})\\d{7})$")){
+        if (!note.getTelMobile().matches("^((\\+?380)([5-9]{1})([0&3-9]{1})\\d{7})$")) {
 
-            errors.rejectValue("telMobile","format.notValid");
+            errors.rejectValue("telMobile", "format.notValid");
         }
 
 
-
-
-
-            if (!note.getEmail().equals(null)&&(!EmailValidator.getInstance().isValid(note.getEmail()))) {
-                errors.rejectValue("email", "email.notValid");
-            }
+        if (!note.getEmail().isEmpty()&&(!EmailValidator.getInstance().isValid(note.getEmail()))) {
+            errors.rejectValue("email", "email.notValid");
         }
+    }
 
 }
